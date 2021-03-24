59c5a1e556ebed5542700f16c161a2db813e8d85
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:18:07 -0600

    Add files via upload

---
+++ Ej20.-CalcularSumaSerieN/Ej20.-CalcularSumaSerieN/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej20._CalcularSumaSerieN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Operacion x = new Operacion();
+            double counter = double.Parse(textBox1.Text);
+            double sum = 1 + x.AddSum(counter);
+            this.textBox2.Text = sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Ej20.-CalcularSumaSerieN/Ej20.-CalcularSumaSerieN/Operacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej20._CalcularSumaSerieN
+{
+    class Operacion
+    {
+        public double AddSum(double n)
+        {
+            double a = 0;
+
+            for (double i = 1; i < n; i++)
+            {
+                if (i % 1 == 0)
+                {
+                    a += i / Math.Pow(2,i);
+                }
+            }
+            return a;
+        }
+    }
+}
